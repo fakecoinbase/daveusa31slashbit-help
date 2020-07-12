1a7253dda143c6a784c42396f55b17745c023011
daveusa31 <41593484+daveusa31@users.noreply.github.com>
2020-07-13 02:36:01 +0300

    Update coinbase propertis

--- bit_help/markets/coinbase.py
+++ bit_help/markets/coinbase.py
@@ -11,18 +11,27 @@ class Coinbase(Client):
 
     def __init__(self, api_key: str=random_api_key, api_secret: str=random_api_secret):
         super().__init__(api_key, api_secret) # Наследуем класс coinbase
-        self.client = Client(api_key, api_secret)
-        self.account_id = self.client.get_primary_account()["id"]
+        self.__client = Client(api_key, api_secret)
+        self.__account_id = self.__client.get_primary_account()["id"]
+
+    @property
+    def client(self):
+        return self.__client
+
+    @property
+    def account_id(self):
+        return self.__account_id
+    
 
     def price(self, currency="USD"):
         currency_pair = "BTC-{}".format(currency.upper()) 
-        price = self.client.get_buy_price(currency_pair=currency_pair)["amount"]
+        price = self.__client.get_buy_price(currency_pair=currency_pair)["amount"]
         return float(price)
 
     def address_balance(self, address_id, confirmations=1):
         balance = 0
 
-        data = self.client.get_address_transactions(self.account_id, address_id)
+        data = self.__client.get_address_transactions(self.__account_id, address_id)
         transactions = data["data"]
 
 
